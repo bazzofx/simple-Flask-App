3ee58231746777e014e76d509d889939e18583f2
unknown <bazzofx@outlook.com>
2024-10-20 19:27:36 +0100

    Added form POST/GET request

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/<nameT>")
-def home(nameT):
-    return render_template("index.html",name=nameT)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
+
+@app.route("/faq.html")
+def FAQ():
+    return render_template("faq.html")
+
+@app.route("/login.html",methods=["GET","POST"])
+def login():
+    if request.method =="POST":
+        user = request.form["url"]
+        return redirect(url_for("user",usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80,debug=True)
+
