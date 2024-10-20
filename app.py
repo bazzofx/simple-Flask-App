8cf956747017c92c1e2229d733322325d388169a
unknown <bazzofx@outlook.com>
2024-10-21 00:40:11 +0100

    Working App version 0.1 Beta

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, request, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/home",methods=["POST","GET"])
+@app.route("/",methods=["POST","GET"])
 def home():
-    return render_template("index.html")
-
-
-@app.route("/faq.html")
-def FAQ():
-    return render_template("faq.html")
-
-@app.route("/login.html",methods=["GET","POST"])
-def login():
-    if request.method =="POST":
-        user = request.form["url"]
-        return redirect(url_for("user",usr=user))
-    else:
-        return render_template("login.html")
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+     return render_template("index.html")
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80,debug=True)
-
